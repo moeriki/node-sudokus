57dad6cfdf6265c4fdded9209be4bad88ac31ddb
Dieter Luypaert <dieterluypaert@gmail.com>
2016-12-30 23:02:12 +0100

    cleaned up CLI usage

--- cli.js
+++ cli.js
@@ -7,20 +7,23 @@
 // modules
 
 const chalk = require('chalk');
-const { map, throttle } = require('lodash/fp');
+const {
+  chunk,
+  flow,
+  getOr,
+  map,
+  noop,
+  parseInt,
+  split,
+  throttle,
+  trim,
+} = require('lodash/fp');
 const logUpdate = require('log-update');
 const { getBorderCharacters, table } = require('table');
-const argv = require('yargs').argv;
 
-const solve = require('./lib');
+const sudokus = require('./lib');
 
 // constants
-
-const TABLE_CONFIG = {
-  border: getBorderCharacters('norc'),
-};
-
-// private variables
 
 const DEMO = [
   [0, 0, 0, 2, 9, 0, 1, 0, 0],
@@ -34,38 +37,109 @@ const DEMO = [
   [0, 4, 3, 0, 2, 0, 0, 8, 7],
 ];
 
+const TABLE_CONFIG = {
+  border: getBorderCharacters('norc'),
+};
+
 // private functions
+
+const createPuzzleTable = (puzzle) => table(puzzle, TABLE_CONFIG);
+
+const updatePuzzle = (puzzle) => {
+  logUpdate(createPuzzleTable(puzzle));
+};
+
+const parseSudoku = flow([
+  split(','),
+  map(flow([
+    trim,
+    getOr('0', '0'),
+    parseInt(10),
+  ])),
+  chunk(9),
+]);
 
 const prettifyCell = (cell) => {
   if (cell.fixed) {
     return chalk.blue(cell.value);
   }
   if (cell.value === 0) {
-    return chalk.gray(0);
+    return chalk.gray('-');
   }
   return String(cell.value);
 };
 
-const prettifyPuzzleState = map(map(prettifyCell));
+const prettifyState = map(map(prettifyCell));
 
-const logPuzzle = (puzzle) => {
-  logUpdate(table(puzzle, TABLE_CONFIG));
-};
+// commands
+
+function solveCommand(argv) {
+  const onProgress = argv.animate
+    ? throttle(200, (state) => {
+      updatePuzzle(prettifyState(state));
+    })
+    : noop
+  ;
+
+  const puzzle = argv.demo ? DEMO : argv.puzzle;
+  const solution = sudokus.solve(puzzle, { onProgress });
+
+  if (argv.animate) {
+    onProgress.cancel();
+    logUpdate.clear();
+  }
+
+  process.stdout.write(
+    argv.json
+      ? JSON.stringify(solution)
+        .replace('[[', '[\n  [')
+        .replace(/,([0-9])/g, ', $1')
+        .replace(/,\[/g, ',\n  [')
+        .replace(']]', ']\n]')
+      : createPuzzleTable(solution)
+    ,
+    { encoding: 'utf8' }
+  );
+}
 
 // cli
 
-const puzzle = argv._[0]
-  ? JSON.parse(argv._[0])
-  : DEMO
+require('yargs') // eslint-disable-line no-unused-expressions
+  .usage('$0 <cmd> [options] [args]')
+  .command(
+    'solve [puzzle]',
+    'solve your sudoku puzzle',
+    (yargs) => yargs
+      .example('solve -ad')
+      .usage('$0 [options] [puzzle]')
+      .options({
+        animate: {
+          alias: 'a',
+          describe: 'animate the solving progress',
+          type: 'boolean',
+        },
+        demo: {
+          alias: 'd',
+          describe: 'solve demo puzzle',
+          type: 'boolean',
+        },
+        json: {
+          alias: 'j',
+          describe: 'output solution as json',
+          type: 'boolean',
+        },
+      })
+      .coerce({
+        puzzle(puzzle) {
+          return puzzle[0] === '['
+            ? JSON.parse(puzzle)
+            : parseSudoku(puzzle)
+          ;
+        },
+      })
+    ,
+    solveCommand
+  )
+  .help()
+  .argv
 ;
-
-const options = {
-  onError: logPuzzle,
-  onProgress: argv.v || argv.visualize ? throttle(200, (state) => {
-    logPuzzle(prettifyPuzzleState(state));
-  }) : undefined,
-};
-
-const solution = solve(puzzle, options);
-
-logPuzzle(solution);
